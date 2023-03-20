50cb6d0b90db4a9b1d91d591432796c4c8b56738
Kevin Cogen <k.cogen@gmail.com>
2023-03-19 22:02:54 -0400

    lint fixes

--- express_server.js
+++ express_server.js
@@ -13,13 +13,13 @@ app.use(cookieSession({
 const bcrypt = require('bcryptjs');
 
 //databases and helpers
-const urlDatabase = {}
+const urlDatabase = {};
 const users = {};
 const { generateRandomString } = require('./helpers');
 const { getUserByEmail } = require('./helpers');
 const { urlsForUser } = require('./helpers');
 
-//first iteration of requests
+// first iteration of requests
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -35,27 +35,9 @@ app.get("/hello", (req, res) => {
 
 //GET /urls/ - render urls_index
 app.get("/urls", (req, res) => {
-  const userURLs = urlsForUser(req.session.user_id, urlDatabase)
-  const templateVars = { urls: userURLs, user: users[req.session['user_id']] };
-  // console.log(urlDatabase)
-  // console.log(userURLs)
-  // console.log(req.session.user_id)
-  res.render("urls_index", templateVars);
-});
-
-//GET /urls/new - route for urls/new
-app.get("/urls/new", (req, res) => {
-  if (req.session.user_id) {
-    const templateVars = { user: users[req.session['user_id']] };
-    res.render("urls_new", templateVars);
-  }
-  res.redirect('/login')
-});
-//GET /urls/:id route for urls/:id
-app.get("/urls/:id", (req, res) => {
   const userURLs = urlsForUser(req.session.user_id, urlDatabase);
-  const templateVars = { urls: userURLs, user: users[req.session.user_id], id: req.params.id }
-  res.render("urls_show", templateVars);
+  const templateVars = { urls: userURLs, user: users[req.session['user_id']] };
+  res.render("urls_index", templateVars);
 });
 
 //POST URLS
@@ -70,15 +52,40 @@ app.post("/urls", (req, res) => {
   urlDatabase[id] = {
     longURL: req.body.longURL,
     userID: user_id
+  };
+  res.redirect(`/urls/${id}`); //issue - doesn't get longURL
+});
+
+//GET /urls/new - route for urls/new
+app.get("/urls/new", (req, res) => {
+  if (req.session.user_id) {
+    const templateVars = { user: users[req.session['user_id']] };
+    res.render("urls_new", templateVars);
   }
-  // console.log(urlDatabase);
-  // res.send("Ok"); // Respond with 'Ok' (we will replace this)'
-  res.redirect(`/urls/${id}`); //issue - doesn't get longURL 
+  res.redirect('/login');
+});
+
+//GET /urls/:id route for urls/:id
+app.get("/urls/:id", (req, res) => {
+  const userURLs = urlsForUser(req.session.user_id, urlDatabase);
+  const templateVars = { urls: userURLs, user: users[req.session.user_id], id: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+// POST URLS/:id - Update long URL
+app.post('/urls/:id', (req, res) => {
+  if (req.session.user_id === urlDatabase[req.params.id].userID) {
+    const longURL = req.body.longURL;
+    urlDatabase[req.params.id].longURL = longURL;
+    res.redirect('/urls');
+  } else {
+    res.status(400).send("Welp, looks like you can't update that URL...");
+  }
 });
 
 //GET u/:id redirect shortURL to long URL.
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id].longURL;
+  const longURL = urlDatabase[req.params.id].longURL; //when not valid longURL, it is undefined?
   if (!longURL) {
     res.status(404).send("Error 404: The requested URL does not exist.");
   } else {
@@ -86,23 +93,13 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
-// POST URLS/:id - Update long URL 
-app.post('/urls/:id', (req, res) => {
-  if (req.session.user_id === urlDatabase[req.params.id].userID) {
-    const longURL = req.body.longURL;
-    urlDatabase[req.params.id].longURL = longURL;
-    res.redirect('/urls');
-  } else {
-    res.status(400).send("Welp, looks like you can't update that URL...");   
-  }
-});
 // POST Delete
 app.post("/urls/:id/delete", (req, res) => {
   if (req.session.user_id === urlDatabase[req.params.id].userID) {
     delete urlDatabase[req.params.id];
     res.redirect("/urls");
   } else {
-    res.status(400).send("Sorry, but that's not a URL you can delete!");   
+    res.status(400).send("Sorry, but that's not a URL you can delete!");
   }
 });
 
@@ -139,8 +136,7 @@ app.get('/registration', (req, res) => {
 });
 
 
-//post endpoint for /register - add to users object
-//include registration errors
+//POST Registration
 app.post('/registration', (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -153,21 +149,19 @@ app.post('/registration', (req, res) => {
     return;
   }
   const hashedPassword = bcrypt.hashSync(password, 10); // hash the password using bcrypt
-  const userID = generateRandomString()
+  const userID = generateRandomString();
   users[userID] = {
     userID,
     email: req.body.email,
     password: hashedPassword // save the hashed password instead of the plain text password
-  }
+  };
   req.session.user_id = userID;
-  console.log(users);
-  console.log(req.body.email);
   res.redirect('/urls');
 });
 
 //get request render login
 app.get('/login', (req, res) => {
-  if (req.session.user_id){
+  if (req.session.user_id) {
     res.redirect('/urls');
     return;
   }
--- helpers.js
+++ helpers.js
@@ -4,14 +4,14 @@ const generateRandomString = () => {
   while (randomString.length < 6) {
     randomString += chars[Math.floor(Math.random() * chars.length)];
   }
-  return randomString
+  return randomString;
 };
 
 const getUserByEmail = (email, users) => {
   for (const userID in users) {
-    const user = users[userID]
+    const user = users[userID];
     if (user.email === email) {
-      return user
+      return user;
     }
   }
   return null;
@@ -20,16 +20,11 @@ const getUserByEmail = (email, users) => {
 const urlsForUser = (userid, database) => {
   const userURL = {};
   for (const id in database) {
-    console.log(id)
-    console.log(userid)
     if (database[id].userID === userid) {
       userURL[id] = database[id];
-      console.log("matches!")
-      console.log(database[id])
-      console.log(userURL[id])
     }
   }
   return userURL;
 };
 
-module.exports = { generateRandomString, getUserByEmail, urlsForUser }
+module.exports = { generateRandomString, getUserByEmail, urlsForUser };
